79e265d35356aa6074d8d3d9ba2cb5c2a896b63b
Koen van der Hauw <koen.vanderhauw@sweco.nl>
2024-03-21 23:00:31 +0100

    update for new functionality to check cells diagonally, to skip inactive/NoData-cells

--- Scripts/IDFbnd.bat
+++ Scripts/IDFbnd.bat
@@ -5,9 +5,8 @@ REM *                                                 *
 REM * IDFbnd.bat                                      *
 REM * DESCRIPTION                                     *
 REM *   Sets BND-values in BND-files at modelboundary *
-REM *   and corrects BND-files for NoData SHD-values. *
 REM * AUTHOR(S): Koen van der Hauw (Sweco)            *
-REM * VERSION: 2.0.0                                  *
+REM * VERSION: 2.0.1                                  *
 REM * MODIFICATIONS                                   *
 REM *   2016-08-01 Initial version                    *
 REM ***************************************************
@@ -17,7 +16,7 @@ REM ***************
 REM * Description *
 REM ***************
 REM IDFbnd sets IDF-values to -1 at the boundary between inactive (0) and active (1) values. 
-REM Ensure that inactive values are defined around the active values in the input grid or set EXTENT-parameter.
+REM Ensures that inactive values are defined around the active values in the input grid and/or along the specified extent
 
 REM ********************
 REM * Script variables *
@@ -25,11 +24,16 @@ REM ********************
 REM SOURCEBNDPATH:   Path to source BND-files
 REM SOURCEBNDFILTER: Filter for boundary IDF-files in SOURCEBNDPATH, e.g. *.IDF
 REM EXTENT:          Extent of the modelboundary (llx,lly,urx,ury), or leave empty to use input extent
+REM CHECKDIAGONALLY: Specify (with value 1) to also check neighbours diagonally during boundary search; this ensures that boundary cells are also placed/kept at diagonals and that boundary at corners is not removed
+REM SKIPINACTIVE:    Specify (with value 1) that inactive (or NoData) cells should not be converted to boundary cells; this can be used if boundary cells in the inner grid area are already set.
+REM                  This effectively ensures that only active cells along the grid or specified extent will changed to boundary, do not use if an inner boundary still needs to be searched.
 REM RESULTPATH:      Path where the scriptresults should be stored
 REM METADATA_SOURCE: Metadata source path or organization, or leave empty. Note: the directory with this script is added automatically to a metadata file
-SET SOURCEBNDPATH=input
+SET SOURCEBNDPATH=tmp
 SET SOURCEBNDFILTER=*.IDF
 SET EXTENT=%MODELEXTENT%
+SET CHECKDIAGONALLY=1
+SET SKIPINACTIVE=1
 SET RESULTPATH=result
 SET METADATA_SOURCE=
 
@@ -45,7 +49,7 @@ REM * Script commands *
 REM *******************
 SETLOCAL EnableDelayedExpansion
 
-TITLE SIF-plus: %SCRIPTNAME%
+TITLE SIF-basis: %SCRIPTNAME%
 
 REM Check that the specified paths exist
 IF NOT EXIST "%SOURCEBNDPATH%" (
@@ -63,7 +67,7 @@ REM Create empty result directory
 IF NOT EXIST "%RESULTPATH%" MKDIR "%RESULTPATH%"
 
 REM Log settings
-SET MSG=Starting %SCRIPTNAME% ...
+SET MSG=Starting script '%SCRIPTNAME%' ...
 ECHO %MSG%
 ECHO %MSG% > %LOGFILE%
 SET MSG=SOURCEBNDPATH: %SOURCEBNDPATH%
@@ -80,12 +84,16 @@ ECHO %MSG%
 ECHO %MSG% >> %LOGFILE%
 
 IF "%SOURCEBNDFILTER%"=="" SET SOURCEBNDFILTER=*.IDF
-SET EXTENTOPTION=/o
-IF DEFINED EXTENT SET EXTENTOPTION=/e:%EXTENT%
+SET OPTIONE=
+SET OPTIOND=
+SET OPTIONI=
+IF DEFINED EXTENT SET OPTIONE=/e:%EXTENT%
+IF "%CHECKDIAGONALLY%"=="1" SET OPTIOND=/d
+IF "%SKIPINACTIVE%"=="1" SET OPTIONI=/i
 
 REM Do boundary corrections with specified options and correction for NoData SHD-valuees
-ECHO "%TOOLSPATH%\IDFbnd.exe" /o %EXTENTOPTION% "%SOURCEBNDPATH%" "%SOURCEBNDFILTER%" 1 -1 0 "%RESULTPATH%" >> %LOGFILE%
-"%TOOLSPATH%\IDFbnd.exe" /o %EXTENTOPTION% "%SOURCEBNDPATH%" "%SOURCEBNDFILTER%" 1 -1 0 "%RESULTPATH%" >> %LOGFILE%
+ECHO "%TOOLSPATH%\IDFbnd.exe" /o %OPTIOND% %OPTIONI% %OPTIONE% "%SOURCEBNDPATH%" "%SOURCEBNDFILTER%" 1 -1 0 "%RESULTPATH%" >> %LOGFILE%
+"%TOOLSPATH%\IDFbnd.exe" /o %OPTIOND% %OPTIONI% %OPTIONE% "%SOURCEBNDPATH%" "%SOURCEBNDFILTER%" 1 -1 0 "%RESULTPATH%" >> %LOGFILE%
 IF ERRORLEVEL 1 GOTO error
 
 REM Add metadata to metadata files with process details that have been created by IDFbnd.exe
@@ -176,4 +184,4 @@ REM FUNCTION: Intialize script and search/call SETTINGS\SIF.Settings.Project.bat
 
 :exit
 ECHO:
-IF "%NOPAUSE%"=="" PAUSE
+IF NOT DEFINED NOPAUSE PAUSE
