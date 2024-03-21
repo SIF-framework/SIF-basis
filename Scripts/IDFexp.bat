5a94fa04d40afa32a243bae9714a642f6ac15d3f
Koen van der Hauw <koen.vanderhauw@sweco.nl>
2024-03-21 23:00:06 +0100

    update for new functionality

--- Scripts/IDFexp.bat
+++ Scripts/IDFexp.bat
@@ -6,7 +6,7 @@ REM * IDFexp.bat                              *
 REM * DESCRIPTION                             *
 REM *   Processes IDF-expressions in INI-file *
 REM * AUTHOR(S): Koen van der Hauw (Sweco)    *
-REM * VERSION: 2.0.0                          *
+REM * VERSION: 2.0.1                          *
 REM * MODIFICATIONS                           *
 REM *   2018-02-28 Initial version            *
 REM *******************************************
@@ -31,98 +31,125 @@ SET DELETETMP=1
 REM *******************
 REM * Tooldescription *
 REM *******************
-REM Usage: IDFexp [/o] [/d] [/i] [/r:d] [/s] [/v:v1] ini outpath
-REM   for evaluating expressions on IDF-files
+REM Usage: IDFexp [/v:[x1]] [/d] [/i] [/m] [/q:[m]] [/r:d] iniFile outPath
 REM outpath - path and for writing all results
-REM ini - text-file with one or more of the following lines:
-REM   <var>=<exp>
-REM     to store the result of IDF-expression <exp> in a variable with name <var>
-REM     variable names follow the rules of filenames and further should not contain operators, decimal seperators
-REM     or other language structures (see below). Variable names are case sensitive.
-REM     variables are stored in memory and also written as IDF-file with filename: <var>.IDF
-REM     variable names can be prefixed by a relative path to write the IDF-file to a subdirectory. example: KHV\100\KHV_L1=KHV+1
-REM     IDF-expressions are defined by one of the following:
-REM     - names of previously defined variables
-REM     - IDF-filename (absolute path or relative to the path of the ini-file
-REM     - floating point constant values
-REM     - NoData to specify NoData-value(s)
-REM     - <exp1> <op> <exp2>
-REM       where <exp1> and <exp2> are (nested) IDF-expressions
-REM       where <op> is an arithmetic operator: ^, *, /, +, -
-REM     - if-expression: if(<cond>,<then>,<else>)
-REM       where <cond> is a condition build up from IDF-expressions and comparison/logical operators
-REM         comparison operators: ==, !=, >, >=, <, <=
-REM         logical operators: &&, ||
-REM         order of evaluation : * or /, + or -, == to <=, && or ||
-REM       where <then> and <else> are IDF-expressions
-REM       extents are enlarged with NoData to union of input IDF-extents
-REM     - min/max-functions: e.g. max(<exp1>,<exp2>)
-REM       to take the minimum/maximum of IDF-expressions <exp1> and <exp2>
-REM       extents are clipped/enlarged with NoData to extent of IDF-expression <exp1>
-REM     - round-function: e.g. round(<exp1>,<decimalcount>)
-REM       where <decimalcount> is an integer for the number of decimals
-REM     - enlarge-function: e.g. enlarge(<exp1>,<exp2>)
-REM       to enlarge IDF-expression <exp1> with NoData to at least the extent of IDF-expression <exp2>
-REM     - clip-function: e.g. clip(<exp1>,<exp2>)
-REM       to clip IDF-expression <exp1> to the extent of IDF-expression <exp2>
-REM     - scale-function: scale(<exp1>,<exp2>[,<method>])
-REM       to scale IDF-expression <exp1> to the cellsize of (IDF-expression) <exp2>. Optional method:
-REM       for downscale: 0=Block (default)
-REM       for upscale: 0=Mean (default), 1=Median, 2=Minimum, 3=Maximum, 4=Most occurring, 5=Boundary
-REM     - bbox-function: bbox(<exp1>)
-REM       to find bounding box with all non-NoData-values; extent is not changed if only NoData-values are present
-REM     parenthesis ('(' and ')') can be used to group subexpressions
-REM     environment variables enclosed by %-symbols will be evaluated
-REM   FOR <i>=<i1> TO <i2>
-REM     to start a FOR-loop with index <i>, that loops from value i1 to i2; lines between FOR and the next ENDFOR-statement are repeated
-REM     the value of index <i> can be accessed by prefixing %%. FOR-loops can be nested. 
-REM     use simple expressions with indices with syntax '%%(i<op><val>)', where <op> is one of '+','-','*' or '/' and <val> an integer value, e.g. C_L%%i=(BOT_L%%i-TOP_L%%(i+1))*KVV_L%%i
-REM     loop values can be padded with zeroes by inserting zeroes after the %% substring: e.g. %%000p will result in values 009,010 and 011 for FOR-loop 'FOR p=9 TO 11'.
-REM   ENDFOR
-REM     to end a FOR-loop, increase index and continue at line after FOR-statement
-REM   #IF [NOT] <cond>: <cmd>
-REM     to use a precondition to check if a command should be executed; command <cmd> is executed
-REM     if precondition '[NOT] <cond>' evaluates to true; the following preconditions are allowed:
-REM     #IF [NOT] EXIST <path>: <cmd>
-REM       check if path <path> exists; surround with double quotes when the path may contain spaces
-REM   REM <comment>
-REM   // <comment>
-REM   ' <comment>
-REM     to define a commentline which will be ignored
-REM
-REM /v:v1 - Use NoData as value v1. Without option v, NoData in one of
-REM         the input IDF-files results in NoData. Without option v1,
-REM         the NoData-value of each IDF-file is used as a value
-REM /d    - Run in debug mode: write intermediate expressions and IDF-files
-REM /i    - Write intermediate results (all IDF-variables) to IDF-files
-REM /m    - Add metadatafiles with (part of) expression(s) and source path
+REM iniFile - Text-file with one or more expressions as defined below
 REM 
-REM Extent corrections. 
+REM Syntax description for expressions in INI-file
+REM -----------------------------------------------
+REM Each line should be one of the following:
+REM - A remark: REM <comment>, to define a commentline which will be ignored
+REM - An empty line, which will be ignored
+REM - An assignment: "<var>=<exp>", to store the result of IDF-expression <exp> in a variable with name <var>
+REM - A precondition followed by an assignment: #IF [NOT] <cond>: <var>=<exp>
+REM   The assignment <var>=<exp> is executed if the precondition '[NOT] <cond>' evaluates to true
+REM   The following preconditions are allowed:
+REM     #IF [NOT] EXIST <path>: <var>=<exp>
+REM       checks if path/file <path> exists; surround with double quotes when the path contains spaces
+REM   notes: optionally use NOT to invert the condition; environment variables may be used
+REM - FOR-loop: FOR <i>=<i1> TO <i2>, to start a FOR-loop with index <i>, that loops from value i1 to i2 and repeats
+REM     lines between FOR and the next ENDFOR-statement. The value of index <i> can be accessed by prefixing %%, which is
+REM     evaluated first. FOR-loops can be nested. Simple expressions with indices are allowed with syntax %% (i<op><val>),
+REM     where <op> is one of '+','-','*' or '/' and <val> is an integer value, e.g. C_L%%i=(BOT_L%%i-TOP_L%%(i+1))*KVV_L%%i
+REM     Loop values can be padded with zeroes by inserting zeroes after the %% substring
+REM     e.g. %%000p will result in values 009,010 and 011 for FOR-loop "FOR p=9 TO 11"
+REM - ENDFOR, to end a FOR-loop, increase index and continue at line after FOR-statement
+REM 
+REM Variable names follow the rules of filenames and should not contain operators, decimal seperators or other
+REM language structures (see below). Variable names are case sensitive.
+REM Variables are stored in memory and also written as IDF-file with filename: <var>.IDF
+REM Variable names can be prefixed by a relative path to write the IDF-file to a subdirectory, e.g. KHV\100\KHV_L1=KHV+0.5
+REM IDF-expressions are defined by one of the following:
+REM - names of previously defined variables
+REM - IDF-filename (absolute path or relative to the path of the ini-file
+REM - floating point constant values
+REM - NoData to specify NoData-value(s)
+REM - <exp1> <op> <exp2>
+REM   where <exp1> and <exp2> are (nested) IDF-expressions
+REM   where <op> is an arithmetic operator: ^, *, /, +, -
+REM - if-expression: if(<cond>,<then>,<else>)
+REM   where <cond> is a condition build up from IDF-expressions and comparison/logical operators
+REM     comparison operators: ==, !=, >, >=, <, <=
+REM     logical operators: &&, ||
+REM     order of evaluation : * or /, + or -, == to <=, && or ||
+REM   where <then> and <else> are IDF-expressions
+REM   extents are enlarged with NoData to union of input IDF-extents
+REM - min/max-functions: min/max(<exp1>,<exp2>)
+REM   to take the minimum/maximum of IDF-expressions <exp1> and <exp2>; extents are clipped/enlarged
+REM   with NoData to extent of IDF-expression <exp1>; cellsize of <exp1> is used for result.
+REM - round-function: round(<exp1>,<decimalcount>)
+REM   where <decimalcount> is an integer for the number of decimals
+REM - enlarge-function: enlarge(<exp1>,<exp2>)
+REM   to enlarge IDF-expression <exp1> with NoData to at least the extent of IDF-expression <exp2>
+REM - clip-function: clip(<exp1>,<exp2>)
+REM   to clip IDF-expression <exp1> to the extent of IDF-expression <exp2>
+REM - scale-function: scale(<exp1>,<exp2>[,<method>])
+REM                   scale(<exp1>,<exp2>,<methodDown>,<methodUp>)
+REM   to scale IDF-expression <exp1> to the cellsize of (IDF-expression) <exp2>. Optional method:
+REM     for downscale: 0=Block (default), 1=Divide
+REM     for upscale: 0=Mean (default), 1=Median, 2=Minimum, 3=Maximum, 4=Most occurring, 5=Boundary, 6=Sum
+REM - bbox-function: bbox(<exp1>)
+REM    to find bounding box with all non-NoData-values; results in NoData-centercell(s) if only NoData-values are present
+REM - cellsize-function: cellsize(<exp1>)
+REM    to retrieve cellsize for some IDF-expression as a (constant) IDF-file
+REM
+REM Notes
+REM -----
+REM - Parenthesis ('(' and ')') can be used to group subexpressions
+REM - In general the cellsize of leftmost expression/IDF-file will be used for result
+REM - In general the ITB-levels of leftmost expression/IDF-file will be used for result
+REM   for if, min and max the first expresion/IDF-file with ITB-levels is used
+REM - Relative paths are evualated as follows: relative to INI-file when reading; relative to outputpath when writing
+REM - Environment variables enclosed by %-symbols will be evaluated
+REM
+REM Extent corrections
+REM ------------------
 REM If EXTENT has not been specified, extent of IDF-files in expressions is corrected in the following cases:
 REM         - if-expression: if(<cond>,<then>,<else>)
 REM           extents are enlarged to union of input IDF-extents
-REM         - min/max-functions: e.g. max(<exp1>,<exp2>)
+REM         - min/max/abs-functions: e.g. max(<exp1>,<exp2>) or abs(<exp>)
 REM           extents are clipped/enlarged to extent of <exp1>
 REM         - clip(<exp1>,<exp2>): clip to extent of <exp2>
 REM         - enlarge(<exp1>,<exp2>): enlarge to at least extent of <exp2>
 REM         - other (+,-,/,*,^,==,!=,<,): no extent corrections
-REM 
-REM example: IDFexp /v:0 "Test\Input" "*L*.IDF" + "Test\Output\file2.IDF"
-REM example INI-file:
-REM 
+REM
+REM Options
+REM -------
+REM /v      - Use NoData as value v1. Without option v, NoData in one of the input IDF-files results in NoData.
+REM           Without option v1, the NoData-value of each IDF-file is used as a value.
+REM /d      - Run in debug mode: write intermediate expressions and IDF-files
+REM /i      - Write intermediate results (all IDF-variables) to IDF-files
+REM /m      - Add metadatafiles with (part of) expression(s) and source path
+REM /q      - Define Quiet-mode for missing IDF-files with one of the following options:
+REM           1) end IDFexp if a missing file is accessed, without raising an error (default);
+REM           2) skip lines or expressions that refer to missing files, without raising an error
+REM /r      - Round values in (intermediate) result IDF-files to d decimals
+REM
+REM Example INI-file
+REM ----------------
+REM REM Read source files
 REM Horst=HORST.IDF
-REM kZUG=50
 REM dL1_ZUG_Horst=dL1_ZUG_Horst.IDF
 REM dL1_ZUG_Slenk=dL1_ZUG_Slenk.IDF
-REM dL1_ZUG=if(Horst==1,dL1_ZUG_Horst,dL1_ZUG_Slenk)
-REM 
-REM kZZG=35
 REM dL1_ZZG_Horst=dL1_ZZG_Horst.IDF
 REM dL1_ZZG_Slenk=dL1_ZZG_Slenk.IDF
-REM dL1_ZZG=if(Horst==1,dL1_ZZG_Horst,dL1_ZZG_Slenk)
 REM 
-REM TX1=dL1_ZUG*kZUG+dL1_ZZG*kZZG
-
+REM REM Define kh-values
+REM kZUG=50
+REM kZZG=35
+REM 
+REM REM Calculate kD-value
+REM dL1_ZUG=if(Horst==1,dL1_ZUG_Horst,dL1_ZUG_Slenk)
+REM dL1_ZZG=if(Horst==1,dL1_ZZG_Horst,dL1_ZZG_Slenk)
+REM kD_L1=dL1_ZUG*kZUG+dL1_ZZG*kZZG
+REM 
+REM Example of nested FOR-loop and indexing
+REM FOR i = 1 TO 2
+REM   FOR j = 3 TO 4
+REM     I%%i = I3 + %%j
+REM     J%%(i + 1) = I3 + %%j
+REM   ENDFOR
+REM ENDFOR
 
 REM *********************
 REM * Derived variables *
@@ -142,7 +169,7 @@ SETLOCAL EnableDelayedExpansion
 TITLE SIF-basis: %SCRIPTNAME%
 
 REM Log settings
-SET MSG=Starting %SCRIPTNAME% ...
+SET MSG=Starting script '%SCRIPTNAME%' ...
 ECHO %MSG%
 ECHO %MSG% > %LOGFILE%
 
@@ -293,4 +320,4 @@ REM FUNCTION: Intialize script and search/call SETTINGS\SIF.Settings.Project.bat
 
 :exit
 ECHO:
-IF "%NOPAUSE%"=="" PAUSE
+IF NOT DEFINED NOPAUSE PAUSE
